03b2d8de91bd7df4e90cea7a03b051b7f97d4491
Prakash Mishra <mishraprakashcpn47@gmail.com>
2025-11-27 01:14:23 +0530

    refactor: Remove `outputFileTracingRoot` configuration and `path` import from Next.js config.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client'], // Remove this line if not using Prisma or similar
-  outputFileTracingRoot: path.join(__dirname, '../../../'),
 };
 
 export default nextConfig;
